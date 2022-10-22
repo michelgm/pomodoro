efd4698609f7bca6fb16cadf9d90c8dcbdfd52b9
Michel Mendonca <michelgm96@outlook.com>
2022-10-21 22:04:44 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/pages/pomodoro.dart';
+import 'package:pomodoro/store/pomodoro_store.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<PomodoroStore>(
+          create: (_) => PomodoroStore(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Pomodoro(),
+      ),
+    );
+  }
+}
+
+
+// import 'package:flutter_mobx/flutter_mobx.dart';
+// import 'package:pomodoro/store/contador_store.dart';
+
+// final store = ContadorStore();
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Contador'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Observer(
+//               builder: (_) => Text(
+//                 '${store.contador}',
+//                 style: Theme.of(context).textTheme.headline4,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: store.incremetar,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
